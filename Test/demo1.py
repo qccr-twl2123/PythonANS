d127e40e56830d355082e044e08d9a5b263e3099
mark1xie <545739504@qq.com>
2017-11-04 16:11:30 +0800

    fix

--- Test/demo1.py
+++ Test/demo1.py
@@ -3,8 +3,9 @@
 
 import pandas as pd
 import tushare as ts
+import  numpy as np
+
 
 df = ts.profit_data(top=60)
 df.sort('shares',ascending=False)
 
-print  df
---
+++ util/CollectionUtil.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.dates as mdates
+import sys
+reload(sys)
+
