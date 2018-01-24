7c8ffadda83e09abed7f70f33de0a4d8408b8caa
mark1xie <545739504@qq.com>
2018-01-24 20:09:36 +0800

    add

--- Test/test15.py
+++ Test/test15.py
@@ -1,30 +1,11 @@
 #!/usr/bin/python
 #coding: utf-8
 
-from scipy.stats import f_oneway
-import  numpy as np
-from scipy import stats
-from statsmodels.formula.api import ols
-from statsmodels.stats.anova import anova_lm
-import  pandas as pd
-from scipy.stats import norm
-
-
+import  tushare as ts
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-def cal_mean(frac):
-    return(0.08*frac+0.15*(1-frac))
 
-mean=list(map(cal_mean,[x/50 for x in range(51)]))
-sd_mat=np.array([list(map(lambda x: math.sqrt((x**2)*0.12**2+
-                                              ((1-x)**2)*0.25**2+2*x*(1-x)*(-1.5+i*0.5)*0.12*0.25),[x/50 for x in range(51)])
-                      ) for i in range(1,6)])
-#[expression for variable in sequence] list comprehension
-plt.plot(sd_mat[0,:],mean,label='-1')
-plt.plot(sd_mat[1,:],mean,label='-0.5')
-plt.plot(sd_mat[2,:],mean,label='0')
-plt.plot(sd_mat[3,:],mean,label='0.5')
-plt.plot(sd_mat[4,:],mean,label='1')
-plt.legend(loc='upper left')
-plt.show()
+
+
+
