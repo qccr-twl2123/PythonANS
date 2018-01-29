d9d33670a9ae9669cdceca06ffa55695d9f14e53
mark1xie <545739504@qq.com>
2018-01-29 19:15:12 +0800

    pandas

--- Pandas教程.md
+++ Pandas教程.md
@@ -54,7 +54,22 @@ df.loc[dates[0],'A']
 df.at[dates[0],'A']
 ```
 
-* 合并
+* 聚合计算
+```python
+import pandas as pd
+
+df = pd.DataFrame({'key1':['a','a','b','b','a'],
+                'key2':['one','two','one','two','one'],
+                'data1':np.random.randn(5),
+                'data2':np.random.randn(5)})
+print df
+# 按key1进行分组
+print df.groupby(df['key1']).mean()
+# 先按key1进行分组,再按key2进行分组
+print df.groupby([df['key1'], df['key2']]).mean()
+# 分组计算,传入自定义函数, key1进行分组,对data1,data2进行max-min差值计算
+print df.groupby('key1')['data1','data2'].agg(lambda arr:arr.max()-arr.min())
+```
 
 * 时间序列
 
--- Test/test15.py
+++ Test/test15.py
@@ -5,6 +5,20 @@ import  tushare as ts
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.DataFrame({'key1':['a','a','b','b','a'],
+                'key2':['one','two','one','two','one'],
+                'data1':np.random.randn(5),
+                'data2':np.random.randn(5)})
+print df
+# 按key1进行分组
+print df.groupby(df['key1']).mean()
+# 先按key1进行分组,再按key2进行分组
+print df.groupby([df['key1'], df['key2']]).mean()
+# 分组计算,传入自定义函数, key1进行分组,对data1,data2进行max-min差值计算
+print df.groupby('key1')['data1','data2'].agg(lambda arr:arr.max()-arr.min())
+
 
 
 
